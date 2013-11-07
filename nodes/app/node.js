875d0729161d262eedeed38057dd6523867133f8
Rob Halff <rob.halff@gmail.com>
2013-11-07 11:51:39 +0100

    make express flows restartable

--- nodes/app/node.js
+++ nodes/app/node.js
@@ -1,2 +1,21 @@
+var app = express();
 
-output = { app: express(), express: express }
+output = { app: app, express: express };
+
+// the process listening doesn't really belong here.
+// but for now it's convenient because it allows to
+// restart the flow, probably restart will only work
+// if you re-start the flow twice.
+// I should expose these events and let one specify
+// what to do during these events.
+
+function close() {
+  app.close();
+}
+
+// on kill 
+process.on('SIGTERM', close);
+
+// on crash
+process.on('uncaughtException', close);
+
