d89937297d94b0df059fd1237b8acdbd44971476
Rob Halff <rob.halff@gmail.com>
2013-11-07 12:43:47 +0100

    move to listen

--- nodes/listen/node.js
+++ nodes/listen/node.js
@@ -1,1 +1,19 @@
-output = { server: input.app.listen(input.port) }
+var server = input.app.listen(input.port);
+output = { server: server }
+
+// the process listening doesn't really belong here.
+// but for now it's convenient because it allows to
+// restart the flow, probably restart will only work
+// if you re-start the flow twice.
+// I should expose these events and let one specify
+// what to do during these events.
+
+function close() {
+  server.close();
+}
+
+// on kill 
+process.on('SIGTERM', close);
+
+// on crash
+process.on('uncaughtException', close);
